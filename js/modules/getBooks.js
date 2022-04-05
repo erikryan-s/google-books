36f1ba9400f564d8fa30fe8f52ba1e0bf66f1857
erik1998serrano <erik1998serrano@outlook.com>
2022-04-05 22:25:26 +1000

    completed home page styling

--- js/modules/getBooks.js
+++
@@ -1,6 +0,0 @@
-const getBooks = async (url, searchTerm) => {
-  const response = await fetch(`${url}+${searchTerm}`);
-  const data = await response.json();
-  return data.items;
-};
-export default getBooks;
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 const background = document.querySelector("#bg-image");
 
 const randomImg = () => {
+    // array of images
     let images = [
         "bg1.jpg",
         "bg2.jpg",
@@ -13,12 +14,16 @@ const randomImg = () => {
         "bg9.jpg",
         "bg10.jpg",
     ];
+    // amount of images
     let numOfImg = images.length;
+    // get random image
     let i = Math.floor(numOfImg * Math.random());
+    // apply image as background
     const urlString = `url(./assets/backgrounds/${images[i]})`;
     background.style.backgroundImage = urlString;
 };
 
+// on refresh
 document.addEventListener("DOMContentLoaded", () => {
     randomImg();
 });
